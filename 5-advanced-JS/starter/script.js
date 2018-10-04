e9e911b9dd472ea19f76af899e33c0652b3b617b
kristenmt <kc7795@hotmail.com>
2018-10-04 11:54:52 -0400

    new lectures

---
+++ 5-advanced-JS/starter/script.js
@@ -0,0 +1,27 @@
+// function constructor
+
+var john = {
+    name: 'john',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
+}
+
+var john = new Person('john', 1990, 'teacher');
+var jane = new Person('jane', 1969, 'designer');
+var mark = new Person('mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+
